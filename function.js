2ec25da438314e3dfec5fe25300ec2b7ab144046
jinhokim <rlawlsgh8113@naver.com>
2023-03-03 22:47:15 +0900

    FEAT: js function

--- function.js
+++ function.js
@@ -57,6 +57,93 @@ function printMessage() {
         console.log(message);
         let childMessage = 'hello';
     }
-    console.log(childMessage);
+    // console.log(childMessage);
 }
 printMessage();
+console.log('============');
+
+// 6. Return a value
+function sum(a, b) {
+    return a + b;
+}
+const res = sum(1, 2);
+console.log(res);
+console.log('============');
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        // long upgrade logic...
+    }
+}
+
+// good
+function upgradeUser(user) {
+    if (user.point <= 10) {
+        return ;
+    }
+    // long upgrade logic...
+}
+
+// 8. First-class function
+// 8.1) Function expression
+// 아래의 경우는 hoisting X
+const print = function () { // anonymous function
+    console.log('print');
+}
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+console.log('============');
+
+// 8.2) Callback function (함수를 전달)
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+
+// anonymous function
+const printYes = function () {
+    console.log("yes!");
+}
+
+// named function
+// better debugging in debugger's stack traces
+// recursions (anonymous는 함수 이름을 몰라서 재귀 X)
+const printNo = function print () {
+    console.log("no!");
+}
+randomQuiz('love you', printYes, printNo);
+randomQuiz('hi', printYes, printNo);
+
+// Arrow function
+// always anonymous
+const tempSimplePrint = function() {
+    console.log('simplePrint!');
+}
+
+const simplePrint = () => console.log('simplePrint!');
+// const add = function (a, b) {
+//     return a + b;
+// }
+const add = (a, b) => a + b;
+
+const simpleMultiply = (a, b) => {
+    return a * b;
+}
+
+simplePrint();
+console.log(add(3, 3));
+console.log(simpleMultiply(3, 3));
+
+// IIFE(Immediately Invoked Function Expression) -> 함수를 바로 실행함
+(function hello() {
+    console.log('IIFE');
+})();
+console.log('============');
